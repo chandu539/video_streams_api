e3036171870f5da30e8f20aba5eff61f81c0a648
chandu539 <chchandu554@gmail.com>
2025-02-10 21:54:34 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -13,11 +13,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: '',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://video-streams-api-frontend.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+
 // MongoDB Connection
 mongoose
   .connect(MONGODB_URI)
